05313caf24e997fe38e2788b405e10d1e69ab445
Untouchab1e <edvard.holst@gmail.com>
2013-03-13 19:10:21 +0100

    Code cleanup and comment additions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	/* This code is executed after the DOM has been completely loaded */
+    /* Increments the download counter */
 
-	$('ul.manager a').click(function(){
-		
+	$('ul.manager a').click(function(){		
 		var countSpan = $('.download-count',this);
 		countSpan.text( parseInt(countSpan.text())+1);
 	});
